8ce7ed90a85effbdcf694e1bbd5e22448569bc13
Seungpyo-nam <67721284+Seungpyo-nam@users.noreply.github.com>
2020-07-02 13:09:10 +0900

    실제로 입력값을 파일에 한줄로 저장

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,6 +1,12 @@
 package codes;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
 
 public class MainDrive {
 	
@@ -85,6 +91,49 @@ public class MainDrive {
 		
 //		변수에 저장한 데이터를 묶어서 파일로 저장
 //		java > 보조기억장치 내보내기 > 파일출력 (save)
+		
+		
+//		어느 파일을 이용할지 파일명 설정
+		File phoneBookFile = new File("phoneBook.txt");
+		
+//		파일 save > 파일 쓰기
+//		파일에 사용자 정보는 (추가)지정 > 기존 내용에서 이어붙이기. (true의 역할)
+		
+		try {
+			
+			FileWriter fw = new FileWriter(phoneBookFile, true);
+//			fw는 개발자가 다루기 불편함. > 보조도구 : bw (buffered witer)
+			
+			BufferedWriter bw  = new BufferedWriter(fw);
+			
+//			실제로 bw를 이용해서 연락처 정보 저장
+			
+//			저장내용 : 3가지 정보를 한 줄로 묶어서
+//			ex. 남승표,0103736125,1991 > 한줄짜리 string으로 저장.
+			
+			String infoStr = String.format("%s,%s,%d", name, phoneNum, birthYear);
+			
+//			묶인 한 줄을 파일에 기록
+			bw.append(infoStr);
+//			파일에 기록하고 나면, 줄이 바뀌지 않는다.
+//			한줄에 한명씩만 저장 예정 > 줄을 바꿔주자
+			bw.newLine();
+			
+			
+//			작업 완료 > 열어둔 bw, fw를 닫아주자.
+			bw.close();
+			fw.close();
+			
+			System.out.println("연락처 저장이 끝났습니다.");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 		
 	}
 
