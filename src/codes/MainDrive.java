da20b0031c2789e38e162cd402e46f3c23f791a2
Seungpyo-nam <67721284+Seungpyo-nam@users.noreply.github.com>
2020-07-02 12:44:37 +0900

    메뉴 입력 값 확인 - 프로그램 종료/잘못된 입력된 대입값 대응
    전화번호 추가 / 조회 기능 구현

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -34,6 +34,29 @@ public class MainDrive {
 			System.out.println("메뉴 선택 : ");
 			int inputMenu = myScan.nextInt();
 			
+//			입력값 확인
+			if(inputMenu ==0) {
+//				프로그램 종료시켜야함 > 무한반복 탈출!
+				
+				System.out.println("전화번호부를 종료합니다.");
+				break;
+				
+			}else if (inputMenu ==1) {
+				
+//				전화번호부 추가 기능
+				
+				
+			}else if( inputMenu == 2) {
+				
+//				전화번호 전체 조회기능
+				
+			}else {
+				
+				System.out.println("잘못된 입력입니다.");
+				System.out.println("다시 입력해주세요.");
+			}
+			
+			
 			
 		}
 		
